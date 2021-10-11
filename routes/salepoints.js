bec0e47c9f4521f249678f60f7795a4207b05e2a
Kevin Oliveira Paiva <kevin.paivaoliveira@heig-vd.ch>
2021-10-11 11:38:43 +0200

    Update salepoints.js

--- routes/salepoints.js
+++ routes/salepoints.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const Salepoint = require('../models/salepoint');
 
 /* get salepointes */
 
 /* get salepoint ID */
-router.get('/salepoints/:id', function(req, res) {
-    res.send('Get item ' + req.params.id);
+router.get('/:id', function(req, res, next) {
+  Salepoint.find().sort('name').exec((err, salepoints) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(salepoints);
+  })
   });
 
 /* post salepoint*/
+router.post('/', (req, res, next) => {
+  const newSalepoint = new Item(req.body);
 
+  newSalepoint.save( (err, savedSalepoint) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(savedSalepoint);
+  })
+});
 
 
 module.exports = router;
