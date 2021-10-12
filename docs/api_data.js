6c406a563b729158cc7988d4fd7315652f9fb483
Kevin Oliveira Paiva <kevin.paivaoliveira@heig-vd.ch>
2021-10-12 11:51:59 +0200

    Ajout d'un exemple apidoc plus avancée
    liste de salepoints et d'un salepoint spécifique

--- docs/api_data.js
+++ docs/api_data.js
@@ -2,44 +2,280 @@ define({ "api": [
   {
     "type": "get",
     "url": "/salepoint/:id",
-    "title": "Request a salepoint's information",
-    "name": "GetSalepoint",
+    "title": "Retrieve a Salepoint",
+    "name": "RetrieveSalepoint",
     "group": "Salepoint",
-    "parameter": {
+    "version": "1.0.0",
+    "description": "<p>Retrieves one Salepoint.</p>",
+    "examples": [
+      {
+        "title": "Example",
+        "content": "GET /salepoint/58b2926f5e1def0123e97281 HTTP/1.1",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "200 OK",
+          "content": "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{\n  \"id\": \"58b2926f5e1def0123e97281\",\n  \"name\": \"Les pommes d'adam\",\n  \"type\": \"Fruits\",\n  \"rating\": 7.4,\n  \"createdAt\": \"1988-07-12T00:00:00.000Z\"\n  \"coordinate\": \"46.7810030625192,6.647229773330583\"\n  \"address\": \"Avenue des sports 20\"\n  \"picture\": \"/img/salepoint1.jpg\"\n  \"paymentMethod\": \"Twint\"\n}",
+          "type": "json"
+        }
+      ],
       "fields": {
-        "Parameter": [
+        "Response body": [
           {
-            "group": "Parameter",
-            "type": "Number",
+            "group": "Response body",
+            "type": "String",
             "optional": false,
             "field": "id",
-            "description": "<p>Unique identifier of the salepoint</p>"
-          }
-        ]
-      }
-    },
-    "success": {
-      "fields": {
-        "Success 200": [
-          {
-            "group": "Success 200",
-            "type": "String",
-            "optional": false,
-            "field": "firstName",
-            "description": "<p>First name of the user</p>"
+            "description": "<p>The unique identifier of the Salepoint</p>"
           },
           {
-            "group": "Success 200",
+            "group": "Response body",
             "type": "String",
             "optional": false,
-            "field": "lastName",
-            "description": "<p>Last name of the user</p>"
+            "field": "name",
+            "description": "<p>The name of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "type",
+            "description": "<p>The type of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "Number",
+            "optional": false,
+            "field": "rating",
+            "description": "<p>How the Salepoint has been rated on a scale of 0 to 10</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "address",
+            "description": "<p>The address of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "picture",
+            "description": "<p>The picture's url of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "createdAt",
+            "description": "<p>The date at which the salepoint was registered</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "paymentMethod",
+            "description": "<p>The payment methode in the salepoint</p>"
           }
         ]
       }
     },
-    "version": "0.0.0",
     "filename": "routes/salepoints.js",
-    "groupTitle": "Salepoint"
+    "groupTitle": "Salepoint",
+    "parameter": {
+      "fields": {
+        "URL path parameters": [
+          {
+            "group": "URL path parameters",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>The unique identifier of the Salepoint to retrieve</p>"
+          }
+        ],
+        "URL query parameters": [
+          {
+            "group": "URL query parameters",
+            "type": "String",
+            "optional": true,
+            "field": "include",
+            "description": "<p>Embed linked resources in the response body:</p> <ul> <li><code>&quot;director&quot;</code> for the Salepoint's director</li> </ul>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "optional": false,
+            "field": "404/NotFound",
+            "description": "<p>No Salepoint was found corresponding to the ID in the URL path</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "404 Not Found",
+          "content": "HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\nNo Salepoint found with ID 58b2926f5e1def0123e97281",
+          "type": "json"
+        }
+      ]
+    }
+  },
+  {
+    "type": "get",
+    "url": "/salepoint/",
+    "title": "list salepoints",
+    "name": "RetrieveSalepoints",
+    "group": "Salepoint",
+    "version": "1.0.0",
+    "description": "<p>Retrieves a paginated list of movies ordered by title (in alphabetical order).</p>",
+    "parameter": {
+      "fields": {
+        "URL query parameters": [
+          {
+            "group": "URL query parameters",
+            "type": "Number",
+            "optional": true,
+            "field": "rating",
+            "description": "<p>Select only salepoints with the specified rating (exact match)</p>"
+          },
+          {
+            "group": "URL query parameters",
+            "type": "Number",
+            "optional": true,
+            "field": "ratedAtLeast",
+            "description": "<p>Select only salepoints with a rating greater than or equal to the specified rating</p>"
+          },
+          {
+            "group": "URL query parameters",
+            "type": "Number",
+            "optional": true,
+            "field": "ratedAtMost",
+            "description": "<p>Select only salepoints with a rating lesser than or equal to the specified rating</p>"
+          },
+          {
+            "group": "URL query parameters",
+            "type": "String",
+            "optional": true,
+            "field": "include",
+            "description": "<p>Embed linked resources in the response body:</p> <ul> <li><code>&quot;director&quot;</code> for the Salepoint's director</li> </ul>"
+          }
+        ],
+        "URL path parameters": [
+          {
+            "group": "URL path parameters",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>The unique identifier of the Salepoint to retrieve</p>"
+          }
+        ]
+      }
+    },
+    "examples": [
+      {
+        "title": "Example",
+        "content": "GET /salepoint?page=2&pageSize=50 HTTP/1.1",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "examples": [
+        {
+          "title": "200 OK",
+          "content": "   HTTP/1.1 200 OK\n   Content-Type: application/json\n\n[\n   {\n     \"id\": \"58b2926f5e1def0123e97281\",\n     \"name\": \"Les pommes d'adam\",\n     \"type\": \"Fruits\",\n     \"rating\": 7.4,\n     \"createdAt\": \"1988-07-12T00:00:00.000Z\"\n     \"coordinate\": \"46.7810030625192,6.647229773330583\"\n     \"address\": \"Avenue des sports 20\"\n     \"picture\": \"/img/salepoint1.jpg\"\n     \"paymentMethod\": \"Twint\"\n   }\n\n   {\n     \"id\": \"58b2926f5e1def0123e97281\",\n     \"name\": \"Les pommes d'adam 2\",\n     \"type\": \"Fruits\",\n     \"rating\": 3.4,\n     \"createdAt\": \"1988-07-12T00:00:00.000Z\"\n     \"coordinate\": \"46.7810030625192,6.647229773330583\"\n     \"address\": \"Avenue des sports 32\"\n     \"picture\": \"/img/salepoint2.jpg\"\n     \"paymentMethod\": \"Twint\"\n   }\n\n   {\n     \"id\": \"58b2926f5e1def0123e97281\",\n     \"name\": \"Les pommes d'adam 3\",\n     \"type\": \"Fruits\",\n     \"rating\": 5.4,\n     \"createdAt\": \"1988-07-12T00:00:00.000Z\"\n     \"coordinate\": \"46.7810030625192,6.647229773330583\"\n     \"address\": \"Avenue des sports 46\"\n     \"picture\": \"/img/salepoint3.jpg\"\n     \"paymentMethod\": \"Twint\"\n   }\n\n ]",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Response body": [
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "id",
+            "description": "<p>The unique identifier of the Salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "name",
+            "description": "<p>The name of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "type",
+            "description": "<p>The type of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "Number",
+            "optional": false,
+            "field": "rating",
+            "description": "<p>How the Salepoint has been rated on a scale of 0 to 10</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "address",
+            "description": "<p>The address of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "picture",
+            "description": "<p>The picture's url of the salepoint</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "createdAt",
+            "description": "<p>The date at which the salepoint was registered</p>"
+          },
+          {
+            "group": "Response body",
+            "type": "String",
+            "optional": false,
+            "field": "paymentMethod",
+            "description": "<p>The payment methode in the salepoint</p>"
+          }
+        ]
+      }
+    },
+    "filename": "routes/salepoints.js",
+    "groupTitle": "Salepoint",
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "type": "Object",
+            "optional": false,
+            "field": "404/NotFound",
+            "description": "<p>No Salepoint was found corresponding to the ID in the URL path</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "404 Not Found",
+          "content": "HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\nNo Salepoint found with ID 58b2926f5e1def0123e97281",
+          "type": "json"
+        }
+      ]
+    }
   }
 ] });
--- routes/salepoints.js
+++ routes/salepoints.js
@@ -5,8 +5,68 @@ const Salepoint = require('../models/salepoint');
 
 
 
-
-/* GET all salepoints (sorted by name ASC) */
+/**
+ * @api {get} /salepoint/ list salepoints 
+ * @apiName RetrieveSalepoints
+ * @apiGroup Salepoint
+ * @apiVersion 1.0.0
+ * @apiDescription Retrieves a paginated list of movies ordered by title (in alphabetical order).
+ *
+ * @apiUse SalepointIdInUrlPath
+ * @apiUse SalepointInResponseBody
+ * @apiUse SalepointIncludes
+ * @apiUse SalepointNotFoundError
+ * 
+ * @apiParam (URL query parameters) {Number} [rating] Select only salepoints with the specified rating (exact match)
+ * @apiParam (URL query parameters) {Number} [ratedAtLeast] Select only salepoints with a rating greater than or equal to the specified rating
+ * @apiParam (URL query parameters) {Number} [ratedAtMost] Select only salepoints with a rating lesser than or equal to the specified rating
+ *
+ * @apiExample Example
+ *     GET /salepoint?page=2&pageSize=50 HTTP/1.1
+ *
+ * @apiSuccessExample 200 OK
+ *     HTTP/1.1 200 OK
+ *     Content-Type: application/json
+ *
+ *  [
+ *     {
+ *       "id": "58b2926f5e1def0123e97281",
+ *       "name": "Les pommes d'adam",
+ *       "type": "Fruits",
+ *       "rating": 7.4,
+ *       "createdAt": "1988-07-12T00:00:00.000Z"
+ *       "coordinate": "46.7810030625192,6.647229773330583"
+ *       "address": "Avenue des sports 20"
+ *       "picture": "/img/salepoint1.jpg"
+ *       "paymentMethod": "Twint"
+ *     }
+ * 
+ *     {
+ *       "id": "58b2926f5e1def0123e97281",
+ *       "name": "Les pommes d'adam 2",
+ *       "type": "Fruits",
+ *       "rating": 3.4,
+ *       "createdAt": "1988-07-12T00:00:00.000Z"
+ *       "coordinate": "46.7810030625192,6.647229773330583"
+ *       "address": "Avenue des sports 32"
+ *       "picture": "/img/salepoint2.jpg"
+ *       "paymentMethod": "Twint"
+ *     }
+ * 
+ *     {
+ *       "id": "58b2926f5e1def0123e97281",
+ *       "name": "Les pommes d'adam 3",
+ *       "type": "Fruits",
+ *       "rating": 5.4,
+ *       "createdAt": "1988-07-12T00:00:00.000Z"
+ *       "coordinate": "46.7810030625192,6.647229773330583"
+ *       "address": "Avenue des sports 46"
+ *       "picture": "/img/salepoint3.jpg"
+ *       "paymentMethod": "Twint"
+ *     }
+ * 
+ *   ]
+ */
 router.get('/', (req, res, next) => {
   Salepoint.find().sort('name').exec((err, salepoints) => {
     if (err) {
@@ -18,16 +78,36 @@ router.get('/', (req, res, next) => {
 });
 
 /**
- * @api {get} /salepoint/:id Request a salepoint's information
- * @apiName GetSalepoint
+ * @api {get} /salepoint/:id Retrieve a Salepoint
+ * @apiName RetrieveSalepoint
  * @apiGroup Salepoint
+ * @apiVersion 1.0.0
+ * @apiDescription Retrieves one Salepoint.
  *
- * @apiParam {Number} id Unique identifier of the salepoint
+ * @apiUse SalepointIdInUrlPath
+ * @apiUse SalepointInResponseBody
+ * @apiUse SalepointIncludes
+ * @apiUse SalepointNotFoundError
  *
- * @apiSuccess {String} firstName First name of the user
- * @apiSuccess {String} lastName  Last name of the user
+ * @apiExample Example
+ *     GET /salepoint/58b2926f5e1def0123e97281 HTTP/1.1
+ *
+ * @apiSuccessExample 200 OK
+ *     HTTP/1.1 200 OK
+ *     Content-Type: application/json
+ *
+ *     {
+ *       "id": "58b2926f5e1def0123e97281",
+ *       "name": "Les pommes d'adam",
+ *       "type": "Fruits",
+ *       "rating": 7.4,
+ *       "createdAt": "1988-07-12T00:00:00.000Z"
+ *       "coordinate": "46.7810030625192,6.647229773330583"
+ *       "address": "Avenue des sports 20"
+ *       "picture": "/img/salepoint1.jpg"
+ *       "paymentMethod": "Twint"
+ *     }
  */
-/* get salepoint ID. */
 router.get('/:id', (req, res, next) => {
   res.send('Get salepoint ' + req.params.id);
 });
@@ -44,6 +124,78 @@ router.post('/', (req, res, next) => {
     res.send(savedSalepoint);
   })
 });
+
+
+/**
+ * @apiDefine SalepointIdInUrlPath
+ * @apiParam (URL path parameters) {String} id The unique identifier of the Salepoint to retrieve
+ */
+
+/**
+ * @apiDefine SalepointInRequestBody
+ * @apiParam (Request body) {String{3..50}} name The name of the Salepoint (must be unique)
+ * @apiParam (Request body) {Number{0..10}} [rating] How the Salepoint has been rated on a scale of 0 to 10
+ */
+
+/**
+ * @apiDefine SalepointInResponseBody
+ * @apiSuccess (Response body) {String} id The unique identifier of the Salepoint
+ * @apiSuccess (Response body) {String} name The name of the salepoint
+ * @apiSuccess (Response body) {String} type The type of the salepoint
+ * @apiSuccess (Response body) {Number} rating How the Salepoint has been rated on a scale of 0 to 10
+ * @apiSuccess (Response body) {String} address The address of the salepoint
+ * @apiSuccess (Response body) {String} picture The picture's url of the salepoint
+ * @apiSuccess (Response body) {String} createdAt The date at which the salepoint was registered
+ * @apiSuccess (Response body) {String} paymentMethod The payment methode in the salepoint
+ */
+
+/**
+ * @apiDefine SalepointIncludes
+ * @apiParam (URL query parameters) {String} [include] Embed linked resources in the response body:
+ * * `"director"` for the Salepoint's director
+ */
+
+/**
+ * @apiDefine SalepointNotFoundError
+ *
+ * @apiError {Object} 404/NotFound No Salepoint was found corresponding to the ID in the URL path
+ *
+ * @apiErrorExample {json} 404 Not Found
+ *     HTTP/1.1 404 Not Found
+ *     Content-Type: text/plain
+ *
+ *     No Salepoint found with ID 58b2926f5e1def0123e97281
+ */
+
+/**
+ * @apiDefine SalepointValidationError
+ *
+ * @apiError {Object} 422/UnprocessableEntity Some of the Salepoint's properties are invalid
+ *
+ * @apiErrorExample {json} 422 Unprocessable Entity
+ *     HTTP/1.1 422 Unprocessable Entity
+ *     Content-Type: application/json
+ *
+ *     {
+ *       "message": "Salepoint validation failed",
+ *       "errors": {
+ *         "title": {
+ *           "kind": "minlength",
+ *           "message": "Path `title` (`0`) is shorter than the minimum allowed length (3).",
+ *           "name": "ValidatorError",
+ *           "path": "title",
+ *           "properties": {
+ *             "message": "Path `{PATH}` (`{VALUE}`) is shorter than the minimum allowed length (3).",
+ *             "minlength": 3,
+ *             "path": "title",
+ *             "type": "minlength",
+ *             "value": "0"
+ *           },
+ *           "value": "0"
+ *         }
+ *       }
+ *     }
+ */
 
 
 module.exports = router;
