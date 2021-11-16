33156bc2194004c12a9be474d1fa63df1c26c667
Kevin Oliveira Paiva <kevin.paivaoliveira@heig-vd.ch>
2021-11-16 23:57:41 +0100

    update final 2

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-11-16T22:55:51.780Z",
+    "time": "2021-11-16T22:56:46.707Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
